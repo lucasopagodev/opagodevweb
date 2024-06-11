f3411a029f6db111d006b4d31596e4c77c153113
Lucas Fernandes Lopes Rodrigues <rodrigues14set@hotmail.com>
2024-06-11 17:22:48 -0300

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/page";
+import Script from "next/script";
 
 const montserrat = Montserrat({ 
   subsets: ["latin"],
@@ -20,6 +21,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-55Q4BH4R');`
+          }}
+        >
+        </Script>
+      </head>
       <body className={montserrat.className}>
         <div className="container mx-auto px-16 py-8">
           <Header />
